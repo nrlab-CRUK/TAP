fd10c773c4ffdaa70e87584c2e7208c58cabd06e
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2022-07-08 17:29:24 +0100

    Missed renamed script.

--- templates/trimming/tagtrim.sh
+++ templates/trimming/tagtrim.sh
@@ -2,7 +2,7 @@ set -eou pipefail
 
 groovy \
 --classpath="!{projectDir}/groovy/tagtrim" \
-"!{projectDir}/groovy/tagtrim/tagtrim2.groovy" \
+"!{projectDir}/groovy/tagtrim/tagtrim.groovy" \
 --read1="!{read1In}" --read2="!{read2In}" \
 --out1="!{read1Out}" --out2="!{read2Out}" \
 --umi1="!{umi1Out}" --umi2="!{umi2Out}"
