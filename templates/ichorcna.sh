9b91a5c3b72ae3eb05f5cb5addf0479328627e58
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2022-09-20 14:58:18 +0100

    Got further with ichor.

--- templates/ichorcna.sh
+++ templates/ichorcna.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-Rscript "!{projectDir}/R/runIchorCNA.R" \
+Rscript --vanilla /opt/conda/envs/nrlab_tap/share/r-ichorcna*/scripts/runIchorCNA.R \
     --id "!{sampleId}_plots" \
     --WIG "!{wiggleFile}" \
     --ploidy "!{params.ICHORCNA_PLOIDY}" \
@@ -15,13 +15,13 @@ Rscript "!{projectDir}/R/runIchorCNA.R" \
     --includeHOMD !{params.ICHORCNA_INCLUDE_HOMD} \
     --chrs "!{params.ICHORCNA_CHROMOSOMES}" \
     --chrTrain "!{params.ICHORCNA_TRAINING_CHROMOSOMES}" \
-    --estimateNormal "!{params.ICHORCNA_ESTIMATE_NORMAL}" \
-    --estimatePloidy "!{params.ICHORCNA_ESTIMATE_PLOIDY}" \
-    --estimateScPrevalence True \
+    --estimateNormal !{params.ICHORCNA_ESTIMATE_NORMAL} \
+    --estimatePloidy !{params.ICHORCNA_ESTIMATE_PLOIDY} \
+    --estimateScPrevalence true \
     --scStates "!{params.ICHORCNA_SUBCLONAL_STATES}" \
     --txnE !{params.ICHORCNA_TXN_E} \
     --txnStrength !{params.ICHORCNA_TXN_STRENGTH} \
-    --outDir ./ \
+    --outDir "./" \
     --plotYLim "!{params.ICHORCNA_PLOT_Y_LIMITS}" \
     --plotFileType "!{params.ICHORCNA_PLOT_FILE_TYPE}" \
     --exons "!{params.ICHORCNA_EXONS}" \
