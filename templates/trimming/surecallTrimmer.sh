85f1affcd431cc44f28929ba1ea4bc2712301ebe
eldrid01 <matthew.d.eldridge@gmail.com>
2022-10-10 12:13:29 +0100

    Add TODO about updating to use the latest AGeNT trimmer

--- templates/trimming/surecallTrimmer.sh
+++ templates/trimming/surecallTrimmer.sh
@@ -1,5 +1,7 @@
 set -eou pipefail
 
+# TODO upgrade to latest version of AGeNT and use -v2 option for SureSelect XT HS2 libraries
+
 java -Xms!{javaMem}m -Xmx!{javaMem}m \
     -jar /opt/AGeNT/SurecallTrimmer_v4.0.1.jar \
      -fq1 "!{read1In}" \
