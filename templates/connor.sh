e07e3c0439718c43785f270d12eeb817fe0a1486
eldrid01 <matthew.d.eldridge@gmail.com>
2022-10-11 12:13:10 +0100

    Added UMT distance threshold setting

--- templates/connor.sh
+++ templates/connor.sh
@@ -9,8 +9,9 @@ AT_LEAST_ONE=$(samtools view "!{bam}" | head -n 1 | wc -l)
 if [[ $AT_LEAST_ONE -gt 0 ]]
 then
     connor -v --force \
-        -s !{params.CONNOR_MIN_FAMILY_SIZE_THRESHOLD} \
-        -f !{params.CONNOR_CONSENSUS_FREQ_THRESHOLD} \
+        --min_family_size_threshold !{params.CONNOR_MIN_FAMILY_SIZE_THRESHOLD} \
+        --consensus_freq_threshold !{params.CONNOR_CONSENSUS_FREQ_THRESHOLD} \
+        --umt_distance_threshold !{params.CONNOR_UMT_DISTANCE_THRESHOLD} \
         --umt_length !{params.CONNOR_UMT_LENGTH} \
         --log_file .command.out \
         "!{bam}" \
