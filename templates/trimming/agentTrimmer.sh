f2976f914029efdf676a01f69e6fcfb399b4aea9
eldrid01 <matthew.d.eldridge@gmail.com>
2022-10-11 12:18:31 +0100

    New template for AGeNT trimmer (replaces older SurecallTrimmer)

---
+++ templates/trimming/agentTrimmer.sh
@@ -0,0 +1,10 @@
+set -eou pipefail
+
+java -Xms!{javaMem}m -Xmx!{javaMem}m \
+    -jar /opt/AGeNT/trimmer.jar \
+     -fq1 "!{read1In}" \
+     -fq2 "!{read2In}" \
+     -out "./!{sampleId}" \
+     -v2 \
+     -minFractionRead !{params.TRIM_MINIMUM_FRACTION_READ} \
+     -qualityTrimming !{params.TRIM_QUALITY_THRESHOLD}
