d3e98f5f2a98ec32fac17c8f11c914487dabb8d4
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2023-01-13 15:12:14 +0000

    Added groovy (again) to the container. Script to remove link and target of the link.

---
+++ groovy/removeInput.groovy
@@ -0,0 +1,49 @@
+#!/usr/bin/env groovy
+
+import java.nio.file.*
+
+/*
+ * Removes a link and the target of the link, if the target is a file or
+ * is missing (broken link).
+ *
+ * Keeps directories (no recursive delete).
+ */
+def remove(path)
+{
+    if (!Files.exists(path, LinkOption.NOFOLLOW_LINKS))
+    {
+        // If it doesn't exist, say it has been removed.
+        // This handles when a call is made to remove a broken symbolic link.
+        return true
+    }
+
+    if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS))
+    {
+        return false
+    }
+
+    if (Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS))
+    {
+        Files.delete(path)
+        return true
+    }
+
+    if (Files.isSymbolicLink(path))
+    {
+        def target = Files.readSymbolicLink(path)
+        def removed = remove(target)
+        if (removed)
+        {
+            Files.delete(path)
+            return true
+        }
+    }
+
+    return false
+}
+
+args.each
+{
+    def file = new File(it)
+    remove(file.toPath())
+}
